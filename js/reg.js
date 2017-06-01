b4454453f56d12fc42af3241af8a118ca4051b03
Tayoji <595521651@qq.com>
2017-06-01 23:00:22 +0800

    #初始化#

---
+++ js/reg.js
@@ -0,0 +1,11 @@
+/**
+ * Created by tangyoujian on 2017/6/1.
+ */
+
+var Reg = React.createClass({
+    render:function () {
+        return (<p className="ssss">
+            ssssssss
+        </p>);
+    }
+})
